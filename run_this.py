ff1b534cacf97e26152bcc82a5458abb149a046f
Wuzhe Xu <wuzhexu@Wuzhes-MacBook-Pro.local>
2020-04-23 19:50:11 -0500

    Merge branch 'master' of /Users/wuzhexu/Desktop/RL_finace_new with conflicts.

--- run_this.py
+++ run_this.py
@@ -2,25 +2,16 @@ from QLtable import QLearningtable
 import numpy as np
 import pandas as pd
 from functions import *
+import sys
+import matplotlib.pyplot as plt
 
-#l= 200
-#data = np.random.rand(1,l)*100
-#d_min, d_max = np.min(data), np.max(data)
-
-data = getStockDataVec('^GSPC')
-#print('this is length:', len(data))
+stock_key, windows_size, n_level, e_num = sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])
+data = getStockDataVec(stock_key)
 d_min, d_max = np.min(data), np.max(data)
-n_level = 4
 data_lvl = np.around([(i-d_min)/((d_max-d_min)/n_level) for i in data])
-data_lvl = data_lvl
-#data_lvl = np.append(data_lvl,'True')
-#print('this is data1 ', data[1], 'this is lvl', data_lvl[2], d_min, d_max)
 actions = [1,2,3]
 RL = QLearningtable(actions)
-e_num = 100
-
-windows_size =5
-start_time = 5
+start_time = windows_size
 
 for i in range(e_num):
     state = get_state(data_lvl, windows_size, start_time)
@@ -30,21 +21,38 @@ for i in range(e_num):
     for t in range(start_time,len(data_lvl)):
         action = RL.act(state)
         sp = get_state(data_lvl, windows_size, t)
-        #print('action is ', action)
         reward = 0
         if action == 1: #buy
-            #print(t, data)
             RL.buyrecord.append(data[t])
         elif action == 2 and len(RL.buyrecord)>0 : #sell
             bought_price = RL.buyrecord.pop(0)
-            #reward = max(data[t]-bought_price,0)
             reward = data[t] - bought_price
             RL.learn(state, sp, action, reward)
             total_profit += data[t]-bought_price
         state = sp
         if sp[-1] == 'True':
             print('End time and totoal profit is ', total_profit)
-#print('Final table max is ', RL.qtable.max())
-#print('Totoal profit is ', total_profit)
+
+time = [i for i in range(len(data_lvl))]
+buy_time = [i for i in range(start_time,len(data_lvl))]
+sell_time = [i for i in range(start_time,len(data_lvl))]
+agent_action = []
+for t in range(start_time,len(data_lvl)):
+    s = get_state(data_lvl, windows_size, t)
+    temp_action = RL.act(s)
+    agent_action.append(temp_action)
+buy_action = data[start_time:].copy()
+sell_action = data[start_time:].copy()
+for i in range(len(buy_action)):
+    if agent_action[i] != 1:
+        buy_action[i] = 0
+    if agent_action[i] !=2:
+        sell_action[i] = 0
 
 
+
+plt.plot(time, data, 'b-', label = 'price')
+plt.plot(buy_time,buy_action,'ro',label = 'buy')
+plt.plot(sell_time,sell_action,'k^',label = 'sell')
+plt.legend()
+plt.show()
